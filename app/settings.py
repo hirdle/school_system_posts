fb67bfa723bbd0a4d6f14d21612037e08237f586
hirdle <ilyapower368@gmail.com>
2022-07-18 10:18:50 +0300

    fixing

--- app/settings.py
+++ app/settings.py
@@ -138,12 +138,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = '/home/c59861/79167744285.na4u.ru/www/static/'
+STATIC_ROOT = '/home/c61097/schoolsystem.na4u.ru/www/static/'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/home/c61097/schoolsystem.na4u.ru/www/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
